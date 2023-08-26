a6279e4d43dd41584d12127d464eb3268c9f5fdb
Andrei Borisov <clinri@ya.ru>
2023-08-26 16:40:21 +0300

    HW 2

--- app/src/main/java/com/borisov/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/borisov/firstcomposeproject/MainActivity.kt
@@ -3,8 +3,11 @@ package com.borisov.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +18,29 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview
+@Composable
+fun Greeting() {
+    val name = "John"
+    Text(text = "Hello $name!", color = Color.Blue)
+}
+
+@Preview
+@Composable
+fun UserInfoPreview() {
+    UserInfo(name = "Vasya", age = 2)
+}
+
 @Composable
 fun UserInfo(name: String, age: Int) {
-    Text(
-        text = "Hello $name! Your are $age years old."
-    )
+    Column {
+        var count = 0
+        while (count<10) {
+            Text(
+                text = "Hello $name! Your are $age years old.",
+                color = Color.Blue
+            )
+            count++
+        }
+    }
 }
