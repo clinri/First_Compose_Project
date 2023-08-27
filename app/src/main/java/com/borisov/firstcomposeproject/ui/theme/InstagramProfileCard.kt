611c7eeedf3716768df35848013b0500b81c60a5
Andrei Borisov <clinri@ya.ru>
2023-08-27 10:27:35 +0300

    HW 4

---
+++ app/src/main/java/com/borisov/firstcomposeproject/ui/theme/InstagramProfileCard.kt
@@ -0,0 +1,68 @@
+package com.borisov.firstcomposeproject.ui.theme
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun InstagramProfileCard() {
+    Row(
+        modifier = Modifier
+            .padding(all = 8.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .size(50.dp)
+                .background(color = Color.Yellow),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(text = "1")
+        }
+        TwoBoxes()
+        TwoBoxes()
+        TwoBoxes()
+    }
+}
+
+@Composable
+private fun TwoBoxes() {
+    Column(
+        modifier = Modifier.height(80.dp),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .size(25.dp)
+                .background(color = Color.Blue),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(text = "3")
+        }
+        Box(
+            modifier = Modifier
+                .size(25.dp)
+                .background(color = Color.Green),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(text = "4")
+        }
+    }
+}
